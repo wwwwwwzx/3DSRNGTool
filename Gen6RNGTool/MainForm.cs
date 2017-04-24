ac1f796634afcc83546b63bc05455e7bf65c776c
wwwwwwzx <togekiss.beyond@gmail.com>
2017-04-24 13:41:04 -0700

    Separate translation function/control grouping

--- Gen6RNGTool/MainForm.cs
+++ Gen6RNGTool/MainForm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using Gen6RNGTool.RNG;
@@ -12,15 +11,6 @@ namespace Gen6RNGTool
     {
         #region global variables
         private string version = "0.10";
-        private static readonly string[] ANY_STR = { "Any", "任意" };
-        private static readonly string[] NONE_STR = { "None", "无" };
-        private static readonly string[] SETTINGERROR_STR = { "Error at ", "出错啦0.0 发生在" };
-        private static readonly string[] FILEERRORSTR = { "Invalid file!", "文件格式不正确" };
-        private static readonly string[,] PIDTYPE_STR =
-        {
-            { "Random", "Nonshiny", "Shiny","Specified"},
-            { "随机", "必不闪", "必闪","特定"}
-        };
 
         private int ver { get { return Gameversion.SelectedIndex; } set { Gameversion.SelectedIndex = value; } }
         private Pokemon[] Pokemonlist;
@@ -35,108 +25,7 @@ namespace Gen6RNGTool
             InitializeComponent();
         }
 
-        #region Controls Grouping
-        private int[] IVup
-        {
-            get { return new[] { (int)ivmax0.Value, (int)ivmax1.Value, (int)ivmax2.Value, (int)ivmax3.Value, (int)ivmax4.Value, (int)ivmax5.Value, }; }
-            set
-            {
-                if (value.Length < 6) return;
-                ivmax0.Value = value[0]; ivmax1.Value = value[1]; ivmax2.Value = value[2];
-                ivmax3.Value = value[3]; ivmax4.Value = value[4]; ivmax5.Value = value[5];
-            }
-        }
-        private int[] IVlow
-        {
-            get { return new[] { (int)ivmin0.Value, (int)ivmin1.Value, (int)ivmin2.Value, (int)ivmin3.Value, (int)ivmin4.Value, (int)ivmin5.Value, }; }
-            set
-            {
-                if (value.Length < 6) return;
-                ivmin0.Value = value[0]; ivmin1.Value = value[1]; ivmin2.Value = value[2];
-                ivmin3.Value = value[3]; ivmin4.Value = value[4]; ivmin5.Value = value[5];
-            }
-        }
-        private int[] BS
-        {
-            get { return new[] { (int)BS_0.Value, (int)BS_1.Value, (int)BS_2.Value, (int)BS_3.Value, (int)BS_4.Value, (int)BS_5.Value, }; }
-            set
-            {
-                if (value.Length < 6) return;
-                BS_0.Value = value[0]; BS_1.Value = value[1]; BS_2.Value = value[2];
-                BS_3.Value = value[3]; BS_4.Value = value[4]; BS_5.Value = value[5];
-            }
-        }
-        private int[] Stats
-        {
-            get { return new[] { (int)Stat0.Value, (int)Stat1.Value, (int)Stat2.Value, (int)Stat3.Value, (int)Stat4.Value, (int)Stat5.Value, }; }
-            set
-            {
-                if (value.Length < 6) return;
-                Stat0.Value = value[0]; Stat1.Value = value[1]; Stat2.Value = value[2];
-                Stat3.Value = value[3]; Stat4.Value = value[4]; Stat5.Value = value[5];
-            }
-        }
-        private NumericUpDown[] EventIV { get { return new[] { EventIV0, EventIV1, EventIV2, EventIV3, EventIV4, EventIV5, }; } }
-        private CheckBox[] EventIVLocked { get { return new[] { Event_IV_Fix0, Event_IV_Fix1, Event_IV_Fix2, Event_IV_Fix3, Event_IV_Fix4, Event_IV_Fix5, }; } }
-        private List<DataGridViewRow> dgvrowlist = new List<DataGridViewRow>();
-        #endregion
-
-        #region Translation and Form Loading
-        private string curlanguage;
-
-        private int lindex { get { return Lang.SelectedIndex; } set { Lang.SelectedIndex = value; } }
-        private static readonly string[] langlist = { "en", "cn" };
-
-        private void ChangeLanguage(object sender, EventArgs e)
-        {
-            string lang = langlist[lindex];
-
-            if (lang == curlanguage)
-                return;
-
-            curlanguage = lang;
-            TranslateInterface(this, curlanguage); // Translate the UI to language.
-            Properties.Settings.Default.Language = curlanguage;
-            Properties.Settings.Default.Save();
-            TranslateInterface(this, lang);
-            Text = Text + $" v{version} @wwwwwwzx";
-
-            StringItem.naturestr = getStringList("Natures", curlanguage);
-            StringItem.hpstr = getStringList("Types", curlanguage);
-            StringItem.species = getStringList("Species", curlanguage);
-            StringItem.genderratio = getStringList("Genderratio", curlanguage);
-
-            for (int i = 0; i < 4; i++)
-                Event_PIDType.Items[i] = PIDTYPE_STR[lindex, i];
-
-            LoadCategory();
-
-            Nature.Items.Clear();
-            Nature.BlankText = ANY_STR[lindex];
-            Nature.Items.AddRange(StringItem.NatureList);
-
-            SyncNature.Items[0] = NONE_STR[lindex];
-            for (int i = 0; i < StringItem.naturestr.Length; i++)
-                Event_Nature.Items[i] = SyncNature.Items[i + 1] = StringItem.naturestr[i];
-
-            for (int i = 1; i < StringItem.species.Length; i++)
-                Event_Species.Items[i] = StringItem.species[i];
-
-            HiddenPower.Items.Clear();
-            HiddenPower.BlankText = ANY_STR[lindex];
-            HiddenPower.Items.AddRange(StringItem.HiddenPowerList);
-
-            GenderRatio.DisplayMember = "Text";
-            GenderRatio.ValueMember = "Value";
-            GenderRatio.DataSource = new BindingSource(StringItem.GenderRatioList, null);
-            GenderRatio.SelectedIndex = 0;
-
-            // display something upon loading
-            Nature.CheckBoxItems[0].Checked = true;
-            Nature.CheckBoxItems[0].Checked = false;
-            HiddenPower.CheckBoxItems[0].Checked = true;
-            HiddenPower.CheckBoxItems[0].Checked = false;
-        }
+        #region Form Loading
         private void MainForm_Load(object sender, EventArgs e)
         {
             DGV.Columns["dgv_Rand"].DefaultCellStyle.Font = new Font("Consolas", 9);
---
+++ Gen6RNGTool/MainForm_CtrlGroup.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Gen6RNGTool
+{
+    public partial class MainForm : Form
+    {
+        private int[] IVup
+        {
+            get { return new[] { (int)ivmax0.Value, (int)ivmax1.Value, (int)ivmax2.Value, (int)ivmax3.Value, (int)ivmax4.Value, (int)ivmax5.Value, }; }
+            set
+            {
+                if (value.Length < 6) return;
+                ivmax0.Value = value[0]; ivmax1.Value = value[1]; ivmax2.Value = value[2];
+                ivmax3.Value = value[3]; ivmax4.Value = value[4]; ivmax5.Value = value[5];
+            }
+        }
+        private int[] IVlow
+        {
+            get { return new[] { (int)ivmin0.Value, (int)ivmin1.Value, (int)ivmin2.Value, (int)ivmin3.Value, (int)ivmin4.Value, (int)ivmin5.Value, }; }
+            set
+            {
+                if (value.Length < 6) return;
+                ivmin0.Value = value[0]; ivmin1.Value = value[1]; ivmin2.Value = value[2];
+                ivmin3.Value = value[3]; ivmin4.Value = value[4]; ivmin5.Value = value[5];
+            }
+        }
+        private int[] BS
+        {
+            get { return new[] { (int)BS_0.Value, (int)BS_1.Value, (int)BS_2.Value, (int)BS_3.Value, (int)BS_4.Value, (int)BS_5.Value, }; }
+            set
+            {
+                if (value.Length < 6) return;
+                BS_0.Value = value[0]; BS_1.Value = value[1]; BS_2.Value = value[2];
+                BS_3.Value = value[3]; BS_4.Value = value[4]; BS_5.Value = value[5];
+            }
+        }
+        private int[] Stats
+        {
+            get { return new[] { (int)Stat0.Value, (int)Stat1.Value, (int)Stat2.Value, (int)Stat3.Value, (int)Stat4.Value, (int)Stat5.Value, }; }
+            set
+            {
+                if (value.Length < 6) return;
+                Stat0.Value = value[0]; Stat1.Value = value[1]; Stat2.Value = value[2];
+                Stat3.Value = value[3]; Stat4.Value = value[4]; Stat5.Value = value[5];
+            }
+        }
+        private NumericUpDown[] EventIV { get { return new[] { EventIV0, EventIV1, EventIV2, EventIV3, EventIV4, EventIV5, }; } }
+        private CheckBox[] EventIVLocked { get { return new[] { Event_IV_Fix0, Event_IV_Fix1, Event_IV_Fix2, Event_IV_Fix3, Event_IV_Fix4, Event_IV_Fix5, }; } }
+        private List<DataGridViewRow> dgvrowlist = new List<DataGridViewRow>();
+    }
+}
