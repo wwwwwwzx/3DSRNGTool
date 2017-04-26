731f238446973d24b290734932d24c948b8dabf7
wwwwwwzx <togekiss.beyond@gmail.com>
2017-04-26 13:47:59 -0700

    Fulfill tinymt method

--- Gen6RNGTool/RNG/TinyMT.cs
+++ Gen6RNGTool/RNG/TinyMT.cs
@@ -2,13 +2,41 @@
 {
     public class TinyMT : IRNG
     {
-        public TinyMT(uint[] status)
+        public uint[] status { get; set; }
+        public uint mat1 { get; } = 0x8f7011ee;
+        public uint mat2 { get; } = 0xfc78ff1f;
+        public uint tmat { get; } = 0x3793fdff;
+
+        private const int MIN_LOOP = 8;
+        private const int PRE_LOOP = 8;
+
+        private const uint TINYMT32_MASK = 0x7FFFFFFF;
+        private const int TINYMT32_SH0 = 1;
+        private const int TINYMT32_SH1 = 10;
+        private const int TINYMT32_SH8 = 8;
+
+        public TinyMT(uint[] st, uint[] para = null)
         {
-            this.status = new uint[4];
-            for (int i = 0; i < 4; i++)
+            st.CopyTo(status,0);
+            if (para != null && para.Length == 3)
             {
-                this.status[i] = status[i];
+                mat1 = para[0];
+                mat2 = para[1];
+                tmat = para[2];
             }
+        }
+
+        public void init(uint seed)
+        {
+            status = new uint[] { seed, mat1, mat2, tmat };
+
+            for (int i = 1; i < MIN_LOOP; i++)
+                status[i & 3] ^= (uint)i + 1812433253 * (status[(i - 1) & 3] ^ (status[(i - 1) & 3] >> 30));
+
+            period_certification();
+
+            for (int i = 0; i < PRE_LOOP; i++)
+                nextState();
         }
 
         public void nextState()
@@ -24,8 +52,8 @@
 
             if ((y & 1) == 1)
             {
-                status[1] ^= param.mat1;
-                status[2] ^= param.mat2;
+                status[1] ^= mat1;
+                status[2] ^= mat2;
             }
         }
 
@@ -37,7 +65,7 @@
             t0 ^= t1;
             if ((t1 & 1) == 1)
             {
-                t0 ^= param.tmat;
+                t0 ^= tmat;
             }
             return t0;
         }
@@ -45,40 +73,26 @@
         #region IRNG Member
         public uint Next()
         {
+            nextState();
             return temper();
         }
 
         public uint Nextuint()
         {
+            nextState();
             return temper();
         }
 
         public void Reseed(uint seed)
         {
-            return; // Todo
+            init(seed);
         }
         #endregion
 
-        private const uint TINYMT32_MASK = 0x7FFFFFFF;
-        private const int TINYMT32_SH0 = 1;
-        private const int TINYMT32_SH1 = 10;
-        private const int TINYMT32_SH8 = 8;
-
-        public uint[] status { get; set; }
-
-        private class TinyMTParameter
+        private void period_certification()
         {
-            public TinyMTParameter(uint mat1, uint mat2, uint tmat)
-            {
-                this.mat1 = mat1;
-                this.mat2 = mat2;
-                this.tmat = tmat;
-            }
-
-            public uint mat1 { get; }
-            public uint mat2 { get; }
-            public uint tmat { get; }
+            if ((status[0] & TINYMT32_MASK) == 0 && status[1] == 0 && status[2] == 0 && status[3] == 0)
+                status = new uint[] { 'T', 'I', 'N', 'Y' };
         }
-        private readonly TinyMTParameter param = new TinyMTParameter(0x8f7011ee, 0xfc78ff1f, 0x3793fdff);
     }
 }
