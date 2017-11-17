459d20fe0efd5aa40aa775fa36152b7f59021524
wwwwwwzx <togekiss.beyond@gmail.com>
2017-11-17 11:11:52 -0800

    Version Bump

--- 3DSRNGTool/Util/Updater.cs
+++ 3DSRNGTool/Util/Updater.cs
@@ -6,7 +6,7 @@ namespace Pk3DSRNGTool
 {
     public static class Updater
     {
-        public static string CurrentVersion = "0.9.7";
+        public static string CurrentVersion = "0.9.7.2";
 
         public static void CheckUpdate()
         {
