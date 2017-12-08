6b745dd2873513c827ee63f99be0989b3d237953
wwwwwwzx <togekiss.beyond@gmail.com>
2017-12-08 02:25:13 -0800

    Mark Sandygast as UM only

--- 3DSRNGTool/Gen7/PKM7.cs
+++ 3DSRNGTool/Gen7/PKM7.cs
@@ -217,7 +217,7 @@
                     new PKM7 { Species = 132, Level = 29, NPC = 08, IVs = new[] {-1,00,-1,31,30,-1}, Ability = 2, Nature = 15, }, // Ditto @ Konikoni City
                     new PKM7 { Species = 132, Level = 29, NPC = 01, IVs = new[] {-1,30,-1,-1,30,31}, Ability = 2, Nature = 10, }, // Ditto @ Konikoni City
                     new PKM7 { Species = 592, Level = 34, NPC = 07, ShinyLocked = true, Ability = 0xFF, Gender = 2, Delay = -4, }, // Frillish @ Route 14
-                    new PKM7 { Species = 769, Level = 30, NPC = 02, ShinyLocked = true, Ability = 0xFF, }, // Sandygast @ Route 15
+                    new PKM7 { Species = 769, Level = 30, NPC = 02, ShinyLocked = true, Ability = 0xFF, Version = GameVersion.UM, }, // Sandygast @ Route 15
                     new PKM7 { Species = 127, NPC = 5, Raining = true, ShinyLocked = true, Ability = 0xFF, Unstable = true},  // Pinsir
                     new PKM7 { Species = 101, Level = 60, Delay = 120, ShinyLocked = true, Unstable = true }, // Electrode @ Team Rocket's Castle
                 }
