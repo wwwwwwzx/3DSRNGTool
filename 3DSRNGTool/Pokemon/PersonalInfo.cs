1311a683e07817692b8e6ddf67f5ce585f4743b0
wwwwwwzx <togekiss.beyond@gmail.com>
2018-01-12 15:57:52 -0800

    Remove personal info setter

--- 3DSRNGTool/Pokemon/PersonalInfo.cs
+++ 3DSRNGTool/Pokemon/PersonalInfo.cs
@@ -3,24 +3,24 @@
     public abstract class PersonalInfo
     {
         protected byte[] Data;
-        public abstract byte[] Write();
-        public abstract int HP { get; set; }
-        public abstract int ATK { get; set; }
-        public abstract int DEF { get; set; }
-        public abstract int SPE { get; set; }
-        public abstract int SPA { get; set; }
-        public abstract int SPD { get; set; }
+        public abstract int HP { get; }
+        public abstract int ATK { get; }
+        public abstract int DEF { get; }
+        public abstract int SPE { get; }
+        public abstract int SPA { get; }
+        public abstract int SPD { get; }
 
         public int[] Stats => new[] { HP, ATK, DEF, SPE, SPA, SPD };
 
-        public abstract int[] Types { get; set; }
-        public abstract int CatchRate { get; set; }
-        public abstract int[] Items { get; set; }
-        public abstract int Gender { get; set; }
-        public abstract int[] EggGroups { get; set; }
-        public abstract int[] Abilities { get; set; }
-        public virtual int FormeCount { get; set; }
-        protected internal virtual int FormStatsIndex { get; set; }
+        public abstract int[] Types { get; }
+        public abstract int CatchRate { get; }
+        public abstract int[] Items { get; }
+        public abstract int Gender { get; }
+        public abstract int[] EggGroups { get; }
+        public abstract int[] Abilities { get; }
+        public abstract byte EscapeRate { get; }
+        public virtual int FormeCount { get; }
+        protected internal virtual int FormStatsIndex { get; }
 
         // Data Manipulation
         public int FormeIndex(int species, int forme)
--- 3DSRNGTool/Pokemon/PersonalInfoORAS.cs
+++ 3DSRNGTool/Pokemon/PersonalInfoORAS.cs
@@ -12,62 +12,21 @@ namespace PKHeX.Core
                 return;
             Data = data;
         }
-        public override byte[] Write()
-        {
-            return Data;
-        }
 
-        public override int HP { get => Data[0x00]; set => Data[0x00] = (byte)value; }
-        public override int ATK { get => Data[0x01]; set => Data[0x01] = (byte)value; }
-        public override int DEF { get => Data[0x02]; set => Data[0x02] = (byte)value; }
-        public override int SPE { get => Data[0x03]; set => Data[0x03] = (byte)value; }
-        public override int SPA { get => Data[0x04]; set => Data[0x04] = (byte)value; }
-        public override int SPD { get => Data[0x05]; set => Data[0x05] = (byte)value; }
-        public override int[] Types
-        {
-            get => new int[] { Data[0x06], Data[0x07] };
-            set
-            {
-                if (value?.Length != 2) return;
-                Data[0x06] = (byte)value[0];
-                Data[0x07] = (byte)value[1];
-            }
-        }
-        public override int CatchRate { get => Data[0x08]; set => Data[0x08] = (byte)value; }
-        public override int Gender { get => Data[0x12]; set => Data[012] = (byte)value; }
-        public override int[] Items
-        {
-            get => new int[] { BitConverter.ToInt16(Data, 0xC), BitConverter.ToInt16(Data, 0xE), BitConverter.ToInt16(Data, 0x10) };
-            set
-            {
-                if (value?.Length != 3) return;
-                BitConverter.GetBytes((short)value[0]).CopyTo(Data, 0xC);
-                BitConverter.GetBytes((short)value[1]).CopyTo(Data, 0xE);
-                BitConverter.GetBytes((short)value[2]).CopyTo(Data, 0x10);
-            }
-        }
-        public override int[] EggGroups
-        {
-            get => new int[] { Data[0x16], Data[0x17] };
-            set
-            {
-                if (value?.Length != 2) return;
-                Data[0x16] = (byte)value[0];
-                Data[0x17] = (byte)value[1];
-            }
-        }
-        public override int[] Abilities
-        {
-            get => new int[] { Data[0x18], Data[0x19], Data[0x1A] };
-            set
-            {
-                if (value?.Length != 3) return;
-                Data[0x18] = (byte)value[0];
-                Data[0x19] = (byte)value[1];
-                Data[0x1A] = (byte)value[2];
-            }
-        }
-        protected internal override int FormStatsIndex { get => BitConverter.ToUInt16(Data, 0x1C); set => BitConverter.GetBytes((ushort)value).CopyTo(Data, 0x1C); }
-        public override int FormeCount { get => Data[0x20]; set => Data[0x20] = (byte)value; }
+        public override int HP => Data[0x00]; 
+        public override int ATK => Data[0x01]; 
+        public override int DEF => Data[0x02]; 
+        public override int SPE => Data[0x03];
+        public override int SPA => Data[0x04];
+        public override int SPD => Data[0x05];
+        public override int[] Types => new int[] { Data[0x06], Data[0x07] };
+        public override int CatchRate  => Data[0x08];
+        public override int Gender => Data[0x12]; 
+        public override int[] Items => new int[] { BitConverter.ToInt16(Data, 0xC), BitConverter.ToInt16(Data, 0xE), BitConverter.ToInt16(Data, 0x10) };
+        public override int[] EggGroups => new int[] { Data[0x16], Data[0x17] };
+        public override int[] Abilities => new int[] { Data[0x18], Data[0x19], Data[0x1A] };
+        public override byte EscapeRate => Data[0x1B];
+        protected internal override int FormStatsIndex  => BitConverter.ToUInt16(Data, 0x1C);
+        public override int FormeCount => Data[0x20];
     }
 }
